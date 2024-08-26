d781440ad170a8e5cb080ab84ef5144631dcd54b
Sachin <sachnaror@gmail.com>
2024-08-27 00:27:07 +0530

    The relentless pursuit of knowledge breeds success

---
+++ README.md
@@ -0,0 +1,1 @@
+#this was created to connect and fed users and passwords from django rest framework https://github.com/sachnaror/Auth-App-Next.js-User-Login-via-DRF-API
--- auth_project/settings.py
+++ auth_project/settings.py
@@ -137,3 +137,6 @@ REST_FRAMEWORK = {
             'rest_framework_simplejwt.authentication.JWTAuthentication',
         ),
     }
+
+
+#this was created to connect and fed users and passwords from django rest framework https://github.com/sachnaror/Auth-App-Next.js-User-Login-via-DRF-API
